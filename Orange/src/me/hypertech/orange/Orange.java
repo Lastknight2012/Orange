885dad6a1d1fc186bed87c3520525c137780e894
lastknight2012 <yangguangpizza@gmail.com>
2013-02-22 15:28:10 +0800

    Orange

---
+++ Orange/src/me/hypertech/orange/Orange.java
@@ -0,0 +1,13 @@
+package me.hypertech.orange;
+
+public class Orange {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Orange/src/me/hypertech/orange/Uploader.java
@@ -0,0 +1,12 @@
+package me.hypertech.orange;
+
+public class Uploader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
